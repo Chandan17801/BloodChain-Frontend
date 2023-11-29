62ffb212e4b945490fe2e53d3852b0e61727003a
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-11-29 16:36:20 +0530

    Hero

---
+++ src/components/UIElements/BlackButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BlackButton({ text, onClick }) {
+  return (
+    <button
+      className="bg-black text-white py-2 px-4 rounded font-semibold"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default BlackButton;
