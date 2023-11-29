34b6b0b51c957e36a1ce99594200e9d1e6b8e249
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-11-29 15:17:07 +0530

    Basic Setup Done

---
+++ src/components/layout.jsx/ResponsiveLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ResponsiveLayout(props) {
+  return (
+    <div className="w-full">
+      <div className="max-w-screen-2xl mx-auto">{props.children}</div>
+    </div>
+  );
+}
+
+export default ResponsiveLayout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import { Inter } from "next/font/google";
+import ResponsiveLayout from "@/components/layout.jsx/ResponsiveLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>Homepage</div>;
+  return (
+    <ResponsiveLayout>
+      <div>Homepage</div>;
+    </ResponsiveLayout>
+  );
 }
