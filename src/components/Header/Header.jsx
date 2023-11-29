3c4a1cf084fd279d75b3f82ddbf669d50aaf661e
Godllolzzz <sudarshanrajpit2002@gamil.com>
2023-11-29 16:38:19 +0530

    header started

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div id="navbar" className="flex justify-between p-4">
+      <div id="navbar-logo"></div>
+      <div>
+        <ul id="navbar-items" className="flex gap-10 text-xl items-center justify-center">
+          <li className="navbar-item">Home</li>
+          <li className="navbar-item">About Us</li>
+          <li className="navbar-item">Find Blood</li>
+          <li className="navbar-item">Donor Login</li>
+          <li className="navbar-item text-red-800 border-2 rounded-md p-2 border-black">Blood Bank Login</li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Header;
