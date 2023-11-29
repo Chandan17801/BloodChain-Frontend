039054b5c55f8853c4554bada49566247a9a7554
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-11-29 15:46:27 +0530

    Basic Setup Done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import { Inter } from "next/font/google";
-import ResponsiveLayout from "@/components/layout.jsx/ResponsiveLayout";
+import ResponsiveLayout from "@/components/layout/ResponsiveLayout";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import Hero from "@/components/Homepage/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <ResponsiveLayout>
-      <div>Homepage</div>;
+      <Header />
+      <Hero />
+      <Footer />
     </ResponsiveLayout>
   );
 }
