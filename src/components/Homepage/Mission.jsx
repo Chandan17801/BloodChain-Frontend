e634e83d854a724b17d623ecc74ec7308b26c7fd
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-11-29 17:28:27 +0530

    K

--- src/components/Homepage/Mission.jsx
+++ src/components/Homepage/Mission.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Mission() {
   return (
-    <div className="container bg-gray-50 p-6">
+    <div className="container mx-auto bg-gray-50 p-6">
       <p className="text-2xl text-center my-4 font-bold">Our Mission</p>
       <p className="text-sm">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
