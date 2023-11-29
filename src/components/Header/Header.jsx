5c458c70abeb978940e6781890fbf9288dd8fcff
Godllolzzz <sudarshanrajpit2002@gamil.com>
2023-11-29 16:58:13 +0530

    Header started once again

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,11 +6,11 @@ function Header() {
       <div id="navbar-logo"></div>
       <div>
         <ul id="navbar-items" className="flex gap-10 text-xl items-center justify-center">
-          <li className="navbar-item">Home</li>
-          <li className="navbar-item">About Us</li>
-          <li className="navbar-item">Find Blood</li>
-          <li className="navbar-item">Donor Login</li>
-          <li className="navbar-item text-red-800 border-2 rounded-md p-2 border-black">Blood Bank Login</li>
+          <li className="navbar-item cursor-pointer hover:border-b-4">Home</li>
+          <li className="navbar-item cursor-pointer hover:border-b-4">About Us</li>
+          <li className="navbar-item cursor-pointer hover:border-b-4">Find Blood</li>
+          <li className="navbar-item cursor-pointer hover:border-b-4">Donor Login</li>
+          <li className="navbar-item text-red-800 border-2 rounded-md p-2 border-gray-400">Blood Bank Login</li>
         </ul>
       </div>
     </div>
