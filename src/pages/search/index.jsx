1aa678d5a2dd533744369500daf9d154ba5f578e
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-11-29 17:57:08 +0530

    K

---
+++ src/pages/search/index.jsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header/Header";
+import ResponsiveLayout from "@/components/layout/ResponsiveLayout";
+import React from "react";
+
+function index() {
+  return (
+    <ResponsiveLayout>
+      <Header />
+    </ResponsiveLayout>
+  );
+}
+
+export default index;
