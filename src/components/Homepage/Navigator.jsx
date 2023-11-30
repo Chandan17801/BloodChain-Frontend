289925c4fa700a2342d763fa458606ce436dee16
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-11-30 13:49:38 +0530

    Css

--- src/components/Homepage/Navigator.jsx
+++ src/components/Homepage/Navigator.jsx
@@ -26,7 +26,7 @@ function Navigator() {
             href="#"
             className="flex font-bold text-xl mx-3 text-center hover:underline"
           >
-            Blood Donation Camp
+            Upcoming Blood Donation Camp
           </Link>
         </div>
         <div className="flex items-center justify-center bg-orange-500 rounded-xl shadow-md w-44 h-40 p-4">
