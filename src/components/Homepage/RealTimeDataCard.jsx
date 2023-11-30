28ad832137edc4bc66ed2d9102ddb5a80fe72545
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-11-30 17:09:29 +0530

    Creative

--- src/components/Homepage/RealTimeDataCard.jsx
+++ src/components/Homepage/RealTimeDataCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function RealTimeDataCard({ text, value, className }) {
   return (
     <div
-      className={`flex flex-col gap-4 py-8 border-red-900 text-white text-center w-56 rounded-xl font-bold items-center justify-center p-4 ${className}`}
+      className={`flex flex-col gap-4 py-8 border-red-900 text-white text-center w-60 rounded-xl font-bold items-center justify-center p-4 ${className}`}
     >
       <div>{text}</div>
       <div>{value}</div>
