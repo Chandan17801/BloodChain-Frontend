1bec4512522061359aa145993301a8e5473579f4
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-11-30 14:21:18 +0530

    Navigator

---
+++ src/components/Homepage/AboutDonation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AboutDonation() {
+  return <div>AboutDonation</div>;
+}
+
+export default AboutDonation;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,16 +6,24 @@ import Hero from "@/components/Homepage/Hero";
 import Mission from "@/components/Homepage/Mission";
 import Navigator from "@/components/Homepage/Navigator";
 import Testimonials from "@/components/Homepage/Testimonials";
+import AboutUs from "@/components/Homepage/AboutUs";
+import Creative from "@/components/Homepage/Creative";
+import WhyDonation from "@/components/Homepage/WhyDonation";
+import HomepageRealTimeData from "@/components/Homepage/HomepageRealTimeData";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <ResponsiveLayout>
-      <Header className="absolute top-0 left-0"/>
+      <Header className="absolute top-0 left-0" />
       <Hero />
-      <Mission />
+      <HomepageRealTimeData />
       <Navigator />
+      <Mission />
+      <Creative />
+      <AboutUs />
+      <WhyDonation />
       <Testimonials />
       <Footer />
     </ResponsiveLayout>
