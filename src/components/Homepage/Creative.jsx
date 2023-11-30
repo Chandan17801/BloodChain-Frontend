be3191b6cf59ae01cd2470cd651f2e9ef5fb81eb
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-11-30 16:29:41 +0530

    Creative

--- src/components/Homepage/Creative.jsx
+++ src/components/Homepage/Creative.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ResponsiveLayout from "../layout/ResponsiveLayout";
+import Image from "next/image";
+import creative from "@/assests/creative.jpg";
 
 function Creative() {
-  return <div>Creative</div>;
+  return (
+    <ResponsiveLayout>
+      <Image src={creative} className="mx-auto" width={700} height={700} />
+    </ResponsiveLayout>
+  );
 }
 
 export default Creative;
