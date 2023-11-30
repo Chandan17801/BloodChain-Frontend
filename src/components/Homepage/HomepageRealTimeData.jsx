2913c8a35baf57ceee1bb7a6d214ec929dddf661
DEVANAND24 <raghavdas90057@gmail.com>
2023-11-30 15:31:19 +0530

    deva

--- src/components/Homepage/HomepageRealTimeData.jsx
+++ src/components/Homepage/HomepageRealTimeData.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import RealTimeDataCard from "./RealTimeDataCard";
 
 function HomepageRealTimeData() {
-  return <div>HomepageRealTimeData</div>;
+  return (
+    <div className="flex p-12 justify-center gap-4 bg-gray-100">
+      <RealTimeDataCard
+        text="Approved Camps"
+        value={100}
+        className="bg-[#B32346]"
+      />
+      <RealTimeDataCard
+        text="Approved Blood Banks"
+        value={100}
+        className="bg-[#991747]"
+      />
+      <RealTimeDataCard
+        text="Donors Registered"
+        value={100}
+        className="bg-[#6A0B37]"
+      />
+      <RealTimeDataCard
+        text="Blood Unit Collected"
+        value={100}
+        className="bg-[#46052D]"
+      />
+    </div>
+  );
 }
 
 export default HomepageRealTimeData;
