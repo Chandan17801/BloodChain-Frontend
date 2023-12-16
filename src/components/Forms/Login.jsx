1db945d63ed459b5da90c2afcf0600c3a70b0a20
Godllolzzz <sudarshanrajpit2002@gamil.com>
2023-12-16 15:53:27 +0530

    login again

--- src/components/Forms/Login.jsx
+++ src/components/Forms/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import BlackButton from "@/components/UIElements/BlackButton";
 import ResponsiveLayout from "@/components/layout/ResponsiveLayout";
 import Link from "next/link";
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header/Header";
 
 
 const login = () => {
-  <Login />
+   return <Login />
 };
 
 export default login;
