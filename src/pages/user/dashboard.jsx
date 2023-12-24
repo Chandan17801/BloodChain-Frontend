e268edb59cb68317f693df01561f92b32b0f1b0f
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-12-24 14:59:33 +0530

    Routing

---
+++ src/pages/user/dashboard.jsx
@@ -0,0 +1,12 @@
+import Dashboard from "@/components/UserDashboard/Dashboard";
+import ResponsiveLayout from "@/components/layout/ResponsiveLayout";
+
+function board() {
+  return (
+    <ResponsiveLayout>
+      <Dashboard />
+    </ResponsiveLayout>
+  );
+}
+
+export default board;
