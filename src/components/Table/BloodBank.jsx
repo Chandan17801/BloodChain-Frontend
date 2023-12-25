c3db1658724b594e180123d7be05f4d07d6735d3
Gaurav Singh <gauravsingh1892000@gmail.com>
2023-12-25 16:54:56 +0530

    searchBar

--- src/components/Table/BloodBank.jsx
+++ src/components/Table/BloodBank.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-function BloodBank() {
-  return <div>Bank</div>;
+function BloodBank({ banks }) {
+  return (
+    <div>
+      {banks.map((bank) => (
+        <div className="header-gradient merri text-white p-4 text-center my-2 flex justify-around">
+          <div>{bank.name}</div>
+          <div>{bank.address}</div>
+          <div>{bank.email}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default BloodBank;
--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,15 +1,24 @@
 import Header from "@/components/Header/Header";
 import BloodBank from "@/components/Table/BloodBank";
 import ResponsiveLayout from "@/components/layout/ResponsiveLayout";
-import React from "react";
+import React, { useState } from "react";
 import Search from "@/components/search/search";
 
 function index() {
+  const [bank, setBank] = useState([
+    { name: "Devanand", address: "gorakhpur", email: "meriemail@email.com" },
+    { name: "chudarshan", address: "gorakhpur", email: "meriemail@email.com" },
+    { name: "Devanand", address: "gorakhpur", email: "meriemail@email.com" },
+    { name: "Devanand", address: "gorakhpur", email: "meriemail@email.com" },
+    { name: "Devanand", address: "bhokal", email: "meriemail@email.com" },
+    { name: "Devanand", address: "gorakhpur", email: "meriemail@email.com" },
+  ]);
+
   return (
     <ResponsiveLayout>
       <Header />
       <Search />
-      <BloodBank />
+      <BloodBank banks={bank} />
     </ResponsiveLayout>
   );
 }
