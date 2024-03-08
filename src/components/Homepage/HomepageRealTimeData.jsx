79867aff74050a0e1b129ce9ce794b8132411af4
Chandan17801 <chandansingh17801@gmail.com>
2024-03-08 11:45:37 +0530

    camps count

--- src/components/Homepage/HomepageRealTimeData.jsx
+++ src/components/Homepage/HomepageRealTimeData.jsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RealTimeDataCard from "./RealTimeDataCard";
+import axios from "axios";
 
 function HomepageRealTimeData() {
+  const [donor, setDonor] = useState();
+  const [bloodbank, setBloodbank] = useState();
+  const [camps, setCamps] = useState();
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        let response = await axios.get(
+          process.env.NEXT_PUBLIC_SERVER_URL + "/users/all"
+        );
+        setDonor(response.data.users.length);
+        response = await axios.get(
+          process.env.NEXT_PUBLIC_SERVER_URL + "/bloodbank/all"
+        );
+        setBloodbank(response.data.users.length);
+        response = await axios.get(
+          process.env.NEXT_PUBLIC_SERVER_URL + "/camps/all"
+        );
+        setCamps(response.data.campaigns.length);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchDetails();
+  }, []);
+
   return (
     <div className="flex p-12 justify-center gap-4 bg-gray-100 mont">
       <RealTimeDataCard
         text="Approved Camps"
-        value={100}
+        value={camps ? camps : 0}
         className="bg-[#B32346]"
       />
       <RealTimeDataCard
         text="Approved Blood Banks"
-        value={100}
+        value={bloodbank ? bloodbank : 0}
         className="bg-[#991747]"
       />
       <RealTimeDataCard
         text="Donors Registered"
-        value={100}
+        value={donor ? donor : 0}
         className="bg-[#6A0B37]"
       />
       <RealTimeDataCard
