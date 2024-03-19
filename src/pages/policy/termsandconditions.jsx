844b639f78e72e36421a82b3eb8eba299da8c800
Godllolzzz <sudarshanrajpit2002@gmail.com>
2024-03-19 23:00:12 +0530

    error

--- src/pages/policy/termsandconditions.jsx
+++ src/pages/policy/termsandconditions.jsx
@@ -6,7 +6,6 @@ import React from "react";
 const BloodChainTermsAndConditions = () => {
   return (
     <ResponsiveLayout>
-      <Header />
       <div className="max-w-2xl mx-auto p-4 text-justify">
         <h2 className="text-3xl font-bold mb-4">Terms and Conditions</h2>
 
