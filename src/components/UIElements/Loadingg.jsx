369ed06578800b085d46df5076fb03b7ccf9640f
Gaurav Singh <gauravsingh1892000@gmail.com>
2024-04-12 00:52:25 +0530

    css

--- src/components/UIElements/Loadingg.jsx
+++ src/components/UIElements/Loadingg.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Loadingg = () => {
   return (
-    <div className="flex min-h-screen max-h-screen gap-4 p-8 bg-[#F7F8FA]">
+    <div className="flex min-h-screen max-h-screen gap-4 p-8 bg-[#c6cddb]">
       <div className="flex-1 flex flex-col gap-4">
-        <div className="flex flex-1 rounded-lg red-creative shadow-gray-300 shadow-md">
+        <div className="flex flex-1 rounded-lg red-creative shadow-gray-700 shadow-md">
           <div className="flex-[2] flex flex-col text-5xl p-4 font-extrabold mont items-center justify-evenly text-white"></div>
           <div className="flex-1 flex flex-col items-end p-4"></div>
         </div>
-        <div className="flex-[2] rounded-lg merri shadow-gray-300 shadow-md p-4 bg-white">
+        <div className="flex-[2] rounded-lg merri shadow-gray-700 shadow-md p-4 bg-gray-300">
           <div
             className="flex flex-col py-2 mont"
             style={{ overflowY: "auto", maxHeight: "400px" }}
@@ -16,10 +16,11 @@ const Loadingg = () => {
         </div>
       </div>
       <div className="flex-[2] flex flex-col gap-4">
-        <div className="flex-1 flex flex-col rounded-lg shadow-gray-300 shadow-md p-4 bg-white gap-4">
+        <div className="flex-1 flex flex-col rounded-lg shadow-gray-700 shadow-md p-4 bg-gray-300 gap-4">
           <div className="flex-1 merri flex justify-between"></div>
         </div>
       </div>
+      
     </div>
   );
 };
